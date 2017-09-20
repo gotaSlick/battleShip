11c91b1bd852b7335d3dff4154e7c561fcd99277
gotaSlick <30895814+gotaSlick@users.noreply.github.com>
2017-09-20 15:27:31 +0200

    guessing

--- app.js
+++ app.js
@@ -40,8 +40,9 @@ var gameState = [
 			tableRow.setAttribute("row", i);
 			for(var j = 0; j < 10; j++) {
 				var tableData = document.createElement("td");
-				tableData.innerHTML = shipData[i][j];
 				tableData.setAttribute("col", j);
+				tableData.setAttribute("onclick", "play(this)");
+				tableData.innerHTML = shipData[i][j];
 				tableRow.appendChild(tableData); //applies tableData(columns) to the tableRows.
 			}
 			gameBoard.appendChild(tableRow); // Outer loop that happens after each row.
@@ -60,6 +61,24 @@ var gameState = [
 					col.innerHTML = gameState[i][j];
 				}
 			}
+		}
+
+		function play(cell) {
+			var col = cell.getAttribute("col");
+			var row = cell.parentElement.getAttribute("row");
+
+			if(gameState[row][col] == null) { // if the game state at position row and col equals zero then
+				if (shipData[row][col] == "X") { // if it's an X it means we have a hit.
+					alert("Hit!");
+				gameState[row][col] = "X";
+				} else {
+					alert("You hit water!");
+					gameState[row][col] = "ha-ha!";
+				}
+			} else {
+				alert("You've already tried this one!");
+			}
+			populateGameBoard(gameState);
 		}
 
 	window.onload = function() {
