226bfe1bf6f263d15bac3d8991956c97771ec504
gotaSlick <30895814+gotaSlick@users.noreply.github.com>
2017-09-20 16:22:15 +0200

    scoring

--- app.js
+++ app.js
@@ -33,22 +33,24 @@ var gameState = [
 	   Patrol Boat - 2 hits
 	*/
 
+
+		
 		function createGameBoard() {
-		var gameBoard = document.getElementById("gameBoard");
-		for (var i = 0; i < 10; i++) {
-			var tableRow = document.createElement("tr");
-			tableRow.setAttribute("row", i);
-			for(var j = 0; j < 10; j++) {
-				var tableData = document.createElement("td");
-				tableData.setAttribute("col", j);
-				tableData.setAttribute("onclick", "play(this)");
-				tableData.innerHTML = shipData[i][j];
-				tableRow.appendChild(tableData); //applies tableData(columns) to the tableRows.
+			var gameBoard = document.getElementById("gameBoard");
+			for (var i = 0; i < 10; i++) {
+				var tableRow = document.createElement("tr");
+				tableRow.setAttribute("row", i);
+				for(var j = 0; j < 10; j++) {
+					var tableData = document.createElement("td");
+					tableData.setAttribute("col", j);
+					tableData.setAttribute("onclick", "play(this)");
+					tableData.innerHTML = shipData[i][j];
+					tableRow.appendChild(tableData); //applies tableData(columns) to the tableRows.
+				}
+				gameBoard.appendChild(tableRow); // Outer loop that happens after each row.
+						// adds up a row and its 10 columns each time the inner loop finishes. This goes 10 times.
 			}
-			gameBoard.appendChild(tableRow); // Outer loop that happens after each row.
-					// adds up a row and its 10 columns each time the inner loop finishes. This goes 10 times.
 		}
-	}
 
 		function populateGameBoard(gameState) {
 			var gameBoard = document.getElementById("gameBoard");
@@ -63,23 +65,45 @@ var gameState = [
 			}
 		}
 
+// If we hit, we want to add 5 points. If we miss, we deduct one.
 		function play(cell) {
-			var col = cell.getAttribute("col");
-			var row = cell.parentElement.getAttribute("row");
+			  var col = cell.getAttribute("col");
+			  var row = cell.parentElement.getAttribute("row");
+			  var score = Number(document.getElementById("score").innerHTML);
+			  var hits = Number(document.getElementById("hits").innerHTML);
 
-			if(gameState[row][col] == null) { // if the game state at position row and col equals zero then
-				if (shipData[row][col] == "X") { // if it's an X it means we have a hit.
-					alert("Hit!");
-				gameState[row][col] = "X";
-				} else {
-					alert("You hit water!");
-					gameState[row][col] = "ha-ha!";
-				}
-			} else {
-				alert("You've already tried this one!");
+			  if (gameState[row][col] == null) { // if the game state at position row and col equals zero then
+			    if (shipData[row][col] == "X") { // if it's an X it means we have a hit.
+			      alert("Hit!");
+			      gameState[row][col] = "X";
+			      score += 5;
+			      hits -= 1;
+			    } else {
+			      alert("You hit water!");
+			      gameState[row][col] = "O";
+			      score -= 1;
+			    }
+			  } else {
+			    alert("You've already tried this one!");
+			  }
+			  document.getElementById("score").innerHTML = score;
+			  document.getElementById("hits").innerHTML = hits;
+			  populateGameBoard(gameState);
+			  hasGameEnded(hits);
 			}
-			populateGameBoard(gameState);
+
+		function hasGameEnded(hits){
+		 	if (hits === 0){
+		     	alert("You won the game");
+		        var tds = document.getElementsByTagName("td");
+		        for (var i = 0; i < tds.length; i++){
+					tds[i].onclick = function(){
+		             	alert("The game has finished!"); 
+		            }
+		        }
+		    }
 		}
+
 
 	window.onload = function() {
 		createGameBoard();
