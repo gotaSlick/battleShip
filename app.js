ffb20a703dcc7c6b5d6dc4209a44f03b88693c0d
gotaSlick <30895814+gotaSlick@users.noreply.github.com>
2017-09-20 14:40:27 +0200

    displayed ships on board

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 var shipData = [
 	[null, null, null, null, null, null, null, null, null, null],
 	[null, null, null, null, null, null, null, null, null, null],
-	[null, 'X', null, null, null, null, null, 'X', null, null],
-	[null, null, 'X', 'X', 'X', 'X', null, null, null, null],
+	['X', 'X', null, null, null, null, null, 'X', 'X', 'X'],
+	[null, null, 'X', 'X', 'X', 'X', 'X', null, null, null],
 	[null, null, null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, 'X', null, null, 'X', null],
+	[null, null, null, null, null, 'X', null, null, 'X', null],
+	[null, null, null, null, null, 'X', null, null, 'X', null],
 	[null, null, null, null, null, null, null, null, 'X', null],
-	[null, null, null, null, null, null, null, null, 'X', null],
-	[null, null, null, null, null, null, null, null, null, null],
-	[null, null, null, null, null, null, null, null, null, null],
 	[null, null, null, null, null, null, null, null, null, null]
 ];
 
